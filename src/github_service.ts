11f39416df7af92191a8948ab50b47a17b5d38e4
Dheeraj Bisht <dheerajbisht876@gmail.com>
2022-07-28 04:47:49 +0530

    API created

--- src/github_service.ts
+++ src/github_service.ts
@@ -1,15 +1,31 @@
 import axios from "axios";
 import { getCurrentTimeStamp } from "./utils";
 
+const _btoa: (data: string, encodeURI?: boolean) => string = (
+  data,
+  encodeURI = false
+) =>
+  encodeURI
+    ? Buffer.from(encodeURIComponent(data)).toString("base64")
+    : Buffer.from(data).toString("base64");
+
+const _atob: (data: string, decodeURI?: boolean) => string = (
+  data,
+  decodeURI = false
+) =>
+  decodeURI
+    ? decodeURIComponent(Buffer.from(data, "base64").toString())
+    : Buffer.from(data, "base64").toString();
+
 /**
  * Takes the file path and content and update the file in the Repo.
  * @param path A string for path of the content to be updated taking the main repo as root
  * @param content The content string, basically the update which is to happen
  */
-const updateContent: (path: string, content: string) => Promise<void> = async (
-  path,
-  content
-) => {
+export const updateContest: (
+  path: string,
+  content: string
+) => Promise<void> = async (path, content) => {
   try {
     const url = `https://api.github.com/repos/${process.env.GITHUB_USERNAME}/${process.env.GITHUB_REPO}/contents/${path}`;
     const getRes = (await axios({
@@ -21,9 +37,7 @@ const updateContent: (path: string, content: string) => Promise<void> = async (
       },
     })) as any;
     const prevData = getRes.data;
-    const base64Content = Buffer.from(encodeURIComponent(content)).toString(
-      "base64"
-    );
+    const base64Content = _btoa(content, true);
     const file = path.split("/").reverse()[0];
     await axios({
       method: "put",
@@ -43,4 +57,29 @@ const updateContent: (path: string, content: string) => Promise<void> = async (
   }
 };
 
-export default updateContent;
+export const getContest: (path: string) => Promise<Object[]> = async (path) => {
+  try {
+    const url = `https://api.github.com/repos/${process.env.GITHUB_USERNAME}/${process.env.GITHUB_REPO}/contents/${path}`;
+    const getRes = (await axios({
+      method: "get",
+      url,
+      headers: {
+        Accept: "application/vnd.github+json",
+        Authorization: `token ${process.env.GITHUB_API_KEY}`,
+      },
+    })) as any;
+    const content = getRes.data["content"];
+    const result = _atob(
+      _btoa(
+        content
+          .split("\n")
+          .map((c: any) => _atob(c))
+          .join("")
+      ),
+      true
+    );
+    return JSON.parse(result);
+  } catch (err: any) {
+    console.log(err.message);
+  }
+};
